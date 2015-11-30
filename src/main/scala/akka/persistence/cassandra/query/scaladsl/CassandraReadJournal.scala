68bd355c89e857285d8c3857016bd067f019cff7
martinzapletal <zapletal-martin@email.cz>
2015-11-29 22:32:08 -0500

    Documentation.

--- src/main/scala/akka/persistence/cassandra/query/scaladsl/CassandraReadJournal.scala
+++ src/main/scala/akka/persistence/cassandra/query/scaladsl/CassandraReadJournal.scala
@@ -265,7 +265,29 @@ class CassandraReadJournal(system: ExtendedActorSystem, config: Config)
         Source.failed(e)
     }
 
-
+  /**
+   * `eventsByPersistenceId` is used to retrieve a stream of events for a particular persistenceId.
+   *
+   * In addition to the `offset` the `EventEnvelope` also provides `persistenceId` and `sequenceNr`
+   * for each event. The `sequenceNr` is the sequence number for the persistent actor with the
+   * `persistenceId` that persisted the event. The `persistenceId` + `sequenceNr` is an unique
+   * identifier for the event.
+   *
+   * The events are ordered by `sequenceNr` and `offset`. Causality is guaranteed (`sequenceNr`s of
+   * events for a particular `persistenceId` are always ordered in a sequence monotonically
+   * increasing by one). Multiple executions of the same bounded stream are guaranteed to
+   * emit exactly the same stream of events.
+   *
+   * `fromSequenceNr` and `toSequenceNr` can be specified to limit the set of returned events.
+   * The `fromSequenceNr` and `toSequenceNr` are inclusive.
+   *
+   * Deleted events are also deleted from the event stream.
+   *
+   * The stream is not completed when it reaches the end of the currently stored events,
+   * but it continues to push new events when new events are persisted.
+   * Corresponding query that is completed when it reaches the end of the currently
+   * stored events is provided by `currentEventsByPersistenceId`.
+   */
   override def eventsByPersistenceId(
       persistenceId: String,
       fromSequenceNr: Long,
@@ -278,6 +300,11 @@ class CassandraReadJournal(system: ExtendedActorSystem, config: Config)
       Some(queryPluginConfig.refreshInterval))
   }
 
+  /**
+   * Same type of query as `eventsByPersistenceId` but the event stream
+   * is completed immediately when it reaches the end of the "result set". Events that are
+   * stored after the query is completed are not included in the event stream.
+   */
   override def currentEventsByPersistenceId(
       persistenceId: String,
       fromSequenceNr: Long,
